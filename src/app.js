5f9175667200cf99de5aab4d6fc821fe166222c1
Dustin Doan <doankhanhdong3105@gmail.com>
2024-09-30 16:41:48 +0700

    add some libraries

---
+++ src/app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const { default: helmet } = require("helmet");
+const morgan = require("morgan");
+const compression = require("compression");
+const app = express();
+
+// init middleware
+app.use(morgan("combined"));
+app.use(helmet());
+app.use(compression());
+
+// init db
+
+// init routes
+app.get("/", (req, res, next) => {
+  const strCompress = "Hello, world";
+  return res.status(200).json({
+    message: "OK",
+  });
+});
+
+// handling errors
+
+module.exports = app;
