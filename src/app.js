6da0de21d7bed7f0e1e84ae8c67a64fcc377c6f9
Dustin Doan <doankhanhdong3105@gmail.com>
2024-11-04 15:17:05 +0700

    connected database using supabase

--- src/app.js
+++ src/app.js
@@ -1,23 +1,58 @@
+// app.js
+
 const express = require("express");
 const { default: helmet } = require("helmet");
 const morgan = require("morgan");
 const compression = require("compression");
+const { createClient } = require("@supabase/supabase-js");
+const cors = require("cors");
+
+require("dotenv").config();
+
 const app = express();
 
-// init middleware
-app.use(morgan("combined"));
+// Initialize middleware
+app.use(cors());
+app.use(morgan("dev"));
 app.use(helmet());
 app.use(compression());
 
-// init db
+// Initialize Supabase client
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
 
-// init routes
-app.get("/", (req, res, next) => {
-  return res.status(200).json({
-    message: "OK",
-  });
+// Health check route
+app.get("/", (req, res) => {
+  return res.status(200).json({ message: "OK" });
 });
 
-// handling errors
+app.get("/rest/v1/projects", async (req, res) => {
+  try {
+    let { data: projects, error } = await supabase.from("projects").select("*");
+
+    if (error) {
+      console.error("Error fetching projects:", error);
+      throw error;
+    }
+
+    res.status(200).json({
+      message: "Projects fetched successfully",
+      data: projects,
+    });
+  } catch (error) {
+    console.error("Caught error:", error);
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Global error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({
+    message: "Internal Server Error",
+    error: err.message,
+  });
+});
 
 module.exports = app;
